0467880f1cf4e6bbd7b997dfdba3db498bcf7004
Pavel Sidorenko <sidorenko@intaro.email>
2025-05-29 15:46:11 +0300

    lesson 9

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,18 +1,6 @@
 import { Application } from '@hotwired/stimulus';
+import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
 
 const app = Application.start();
-
-// Автоподгрузка контроллеров
-const context = require.context(
-    'controllers',
-    true,
-    /\.(j|t)s$/
-);
-
-context.keys().forEach(key => {
-    const module = context(key);
-    const name = key.replace(/^\.\//, '')
-        .replace(/\.(j|t)s$/, '')
-        .replace(/\//g, '--');
-    app.register(name, module.default);
-});
+const context = require.context('./controllers', true, /\.(j|t)s$/);
+app.load(definitionsFromContext(context));
